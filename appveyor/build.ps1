c6fad7edc64adaddf7cd7b02cb57bdd8e256381e
Kim Birkelund <kim@birkelund.me>
2018-07-28 23:15:29 +0200

    Refactor build scripts.

--- appveyor/build.ps1
+++ appveyor/build.ps1
@@ -1,7 +1,40 @@
-dotnet restore src; 
+# Restore pacakges
+dotnet restore src;
 checkLastExitCode;
 
+
+#Build code
 dotnet build src `
     --no-restore `
-    --configuration Release; 
+    --configuration Release;
 checkLastExitCode;
+
+
+# Create module folder
+$name = $env:APPVEYOR_PROJECT_NAME;
+
+Remove-Item $name -Recurse -Force -ErrorAction SilentlyContinue;
+New-Item -ItemType Directory $name;
+
+Copy-Item .\src\PSFormatDeepString\bin\Release\netstandard2.0\* $name;
+
+$prereleaseVersion = .\nbgv get-version -f json | `
+    ConvertFrom-Json | `
+    Select-Object -ExpandProperty PrereleaseVersion;
+
+$simpleVersion = .\nbgv get-version -f json |
+    ConvertFrom-Json |
+    Select-Object -ExpandProperty CloudBuildAllVars |
+    Select-Object -ExpandProperty NBGV_SimpleVersion;
+
+New-ModuleManifest `
+    -Path (Join-Path $name "$name.psd1") `
+    -Guid "$env:ModuleGuid" `
+    -Author "$env:APPVEYOR_ACCOUNT_NAME" `
+    -Copyright "(c) 2018 $env:APPVEYOR_ACCOUNT_NAME. All rights reserved." `
+    -RootModule "$name.dll" `
+    -ModuleVersion $simpleVersion `
+    -Description (Get-Content -Raw .\README.md) `
+    -CmdletsToExport "*" `
+    -LicenseUri "https://github.com/$($env:APPVEYOR_REPO_NAME)/blob/master/LICENSE" `
+    -PrivateData @{ Prerelease = $prereleaseVersion };
--- appveyor/master_deploy_scripts.ps1
+++ appveyor/master_deploy_scripts.ps1
@@ -4,36 +4,10 @@ PARAM()
 $ErrorActionPreference = "Stop";
 Set-StrictMode -Version Latest;
 
-.\nbgv tag; 
+.\nbgv tag;
 checkLastExitCode;
 
 $name = $env:APPVEYOR_PROJECT_NAME;
-
-Remove-Item $name -Recurse -Force -ErrorAction SilentlyContinue;
-New-Item -ItemType Directory $name;
-
-Copy-Item .\src\PSFormatDeepString\bin\Release\netstandard2.0\* $name;
-
-$prereleaseVersion = .\nbgv get-version -f json | `
-    ConvertFrom-Json | `
-    Select-Object -ExpandProperty PrereleaseVersion;
-
-$simpleVersion = .\nbgv get-version -f json | 
-    ConvertFrom-Json | 
-    Select-Object -ExpandProperty CloudBuildAllVars | 
-    Select-Object -ExpandProperty NBGV_SimpleVersion;
-
-New-ModuleManifest `
-    -Path (Join-Path $name "$name.psd1") `
-    -Guid "$env:ModuleGuid" `
-    -Author "$env:APPVEYOR_ACCOUNT_NAME" `
-    -Copyright "(c) 2018 $env:APPVEYOR_ACCOUNT_NAME. All rights reserved." `
-    -RootModule "$name.dll" `
-    -ModuleVersion $simpleVersion `
-    -Description (Get-Content -Raw .\README.md) `
-    -CmdletsToExport "*" `
-    -LicenseUri "https://github.com/$($env:APPVEYOR_REPO_NAME)/blob/master/LICENSE" `
-    -PrivateData @{ Prerelease = $prereleaseVersion };
 
 Publish-Module `
     -NuGetApiKey $env:PowerShellGalleryApiKey `
@@ -41,5 +15,5 @@ Publish-Module `
     -Repository PSGallery `
     -Verbose;
 
-git push --tags; 
+git push --tags;
 checkLastExitCode;

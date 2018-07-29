e6d688a42c1f865a30b00604b932860ee9f45553
Kim Birkelund <kim@birkelund.me>
2018-07-29 19:46:32 +0200

    Deploy develop branch, allowing prerelease.

---
+++ appveyor/develop_deploy_scripts.ps1
@@ -0,0 +1,16 @@
+[CmdletBinding()]
+PARAM()
+
+$ErrorActionPreference = "Stop";
+Set-StrictMode -Version Latest;
+
+
+Write-Host "Publish module"
+Publish-Module `
+    -NuGetApiKey $env:PowerShellGalleryApiKey `
+    -Path $env:APPVEYOR_PROJECT_NAME `
+    -Repository PSGallery `
+    -AllowPrerelease:$false
+    -Verbose;
+Write-Host "/Publish module"
+

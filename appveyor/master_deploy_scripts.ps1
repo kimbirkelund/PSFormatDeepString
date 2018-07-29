08ac0fdb29bcc40f7653bdd82aea9961364f435d
Kim Birkelund <kim@birkelund.me>
2018-07-29 19:46:17 +0200

    Skip tags, require non-prerelease version for master builds.

--- appveyor/master_deploy_scripts.ps1
+++ appveyor/master_deploy_scripts.ps1
@@ -5,22 +5,12 @@ $ErrorActionPreference = "Stop";
 Set-StrictMode -Version Latest;
 
 
-Write-Host "Create tag"
-.\nbgv tag;
-checkLastExitCode;
-Write-Host "/Create tag"
-
-
 Write-Host "Publish module"
 Publish-Module `
     -NuGetApiKey $env:PowerShellGalleryApiKey `
     -Path $env:APPVEYOR_PROJECT_NAME `
     -Repository PSGallery `
+    -AllowPrerelease:$false
     -Verbose;
 Write-Host "/Publish module"
 
-
-Write-Host "Push tags"
-git push origin --tags;
-checkLastExitCode;
-Write-Host "/Push tags"

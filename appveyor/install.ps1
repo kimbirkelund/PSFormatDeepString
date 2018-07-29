26fa03cac151611ea2312d50a98d6adf7c85e653
Kim Birkelund <kim@birkelund.me>
2018-07-29 18:58:07 +0200

    Clean up.

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -9,8 +9,8 @@ function checkLastExitCode
 {
     if ($LASTEXITCODE -ne 0)
     {
-        Write-Error "Error in last command. Exit code was $LASTEXITCODE, which is -ne 0." 
-    } 
+        Write-Error "Error in last command. Exit code was $LASTEXITCODE, which is -ne 0."
+    }
 }
 
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
@@ -18,8 +18,8 @@ Update-Module PowerShellGet -RequiredVersion 1.6 -Confirm:$false -Force;
 
 dotnet tool install `
     --tool-path . `
-    nbgv; 
+    nbgv;
 checkLastExitCode;
 
-.\nbgv cloud; 
+.\nbgv cloud;
 checkLastExitCode;
--- appveyor/master_deploy_scripts.ps1
+++ appveyor/master_deploy_scripts.ps1
@@ -4,16 +4,19 @@ PARAM()
 $ErrorActionPreference = "Stop";
 Set-StrictMode -Version Latest;
 
+
+# Create tag
 .\nbgv tag;
 checkLastExitCode;
 
-$name = $env:APPVEYOR_PROJECT_NAME;
 
+# Publish module
 Publish-Module `
     -NuGetApiKey $env:PowerShellGalleryApiKey `
-    -Path (Resolve-Path $name) `
+    -Path $env:APPVEYOR_PROJECT_NAME `
     -Repository PSGallery `
     -Verbose;
+
 
 git push --tags;
 checkLastExitCode;
